62964a6d50eb2e77b857bcfe3586c9f6826ccb7b
Demet Demir <demir.demet.453@gmail.com>
2018-02-22 21:32:06 +0100

    Initial Commit

---
+++ Hello_World1/src/Hello_World1.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : Hello_World.cpp
+// Author      : Demet Demir
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//<- This is a comment
+/* or this for more
+than one line comment
+*/
+
+//includes the iostream library
+#include <iostream>
+
+//namespace std for now
+using namespace std;
+
+// Main function where the Program executes
+int main() {
+	//output Stream between " ", endl for the end of the line
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
