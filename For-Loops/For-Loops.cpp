fed27daf8cbe9227d5aa5b2765a7d47d59ee485d
Demet Demir <demir.demet.453@gmail.com>
2018-02-24 20:33:26 +0100

    Initial commit

---
+++ For-Loops/For-Loops.cpp
@@ -0,0 +1,35 @@
+/*
+ * For-Loops.cpp
+ *
+ *  Created on: 24.02.2018
+ *      Author: Demet
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//for loop for counting to 10
+	//first you have to create and initialize an variable as an increment (int i=0;
+	// then you have your statement, so the loop ends when the statement is true
+	//i++ increments i every time the loop passes through
+	for(int i=0; i <10; i++)
+	{
+		cout << i << endl;
+	}
+
+
+	//print a message 10 times
+	for(int i=0; i <10; i++)
+	{
+		cout << "Print this 10 times!" << endl;
+	}
+
+
+	return 0;
+
+}
+
