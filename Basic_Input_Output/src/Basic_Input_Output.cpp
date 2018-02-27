50a4bdca499eb41d5e27b28c276e3d8be02f1223
Demet Demir <demir.demet.453@gmail.com>
2018-02-27 10:55:36 +0100

    Initial commit

---
+++ Basic_Input_Output/src/Basic_Input_Output.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Basic_Input_Output.cpp
+// Author      : Demet Demir
+// Description : Basic Input/Output in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	//Input
+	string variable1;
+	cin >> variable1;
+	//Output
+	cout << "!!!Hello " << variable1 << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
