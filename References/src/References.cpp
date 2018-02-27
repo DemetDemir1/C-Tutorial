9c4ce0c34d2804f0b03c0819ca9c5bb4f3d5d2ea
Demet Demir <demir.demet.453@gmail.com>
2018-02-27 11:03:37 +0100

    Initial commit

---
+++ References/src/References.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : References.cpp
+// Author      : Demet Demir
+// Version     :
+// Copyright   : 
+// Description : References in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	   // declare simple variables
+	   int    i;
+	   double d;
+
+	   // declare reference variables
+	   int&    r = i;
+	   double& s = d;
+
+	   i = 5;
+	   cout << "Value of i : " << i << endl;
+	   cout << "Value of i reference : " << r  << endl;
+
+	   d = 11.7;
+	   cout << "Value of d : " << d << endl;
+	   cout << "Value of d reference : " << s  << endl;
+	return 0;
+}
